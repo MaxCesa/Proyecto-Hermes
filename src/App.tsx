d87a4b194b34cfc94efb97ef54984b7791a7c91e
MaxCesa <maxicesa00@gmail.com>
2024-03-29 20:20:52 -0300

    Correxion integracion de Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginPage from "./Pages/LoginPage.tsx";
+
+function App() {
+  return (
+    <div>
+      <LoginPage></LoginPage>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div className="bg-gray-800 h-[100vh]">
+      <h1 className="text-gray-100">Login</h1>
+    </div>
+  );
+};
+
+export default LoginPage;
