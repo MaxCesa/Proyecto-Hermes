71f74c275ce650865fd4cd468a9dca11d41a89a8
MaxCesa <maxicesa00@gmail.com>
2024-04-02 03:13:53 -0300

    Diseño login Cyberpunk

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -2,8 +2,25 @@ import React from "react";
 
 const LoginPage = () => {
   return (
-    <div className="bg-gray-800 h-[100vh]">
-      <h1 className="text-gray-100">Login</h1>
+    <div className="bg-CPblack h-[100vh] flex items-center justify-center p-100">
+      {/*login*/}
+      <div className="w-full md:w-[450px]">
+        <h1 className="text-CPyellow cyber-glitch-4 text-center cyberpunk-font text-4xl md:text-6xl mb-10">
+          Hermes
+        </h1>
+        <div className="flex flex-col gap-3 bg-transparent cyber-glitch-2 p-6 min-h-[150px] border-CPred border-8">
+          <input
+            type="text"
+            placeholder="Ingrese nombre"
+            className="flex-1 bg-transparent px-3 py-1 border-2 border-CPred rounded-full text-CPwhite"
+          />
+          <input
+            type="text"
+            placeholder="Ingrese contraseña"
+            className="flex-1 bg-transparent px-3 py-1 border-2 border-CPred rounded-full"
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ const autoprefixer = require("autoprefixer");
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        CPyellow: "#f8ef02",
+        CPcyan: "#00ffd2",
+        CPred: "#ff003c",
+        CPblue: "#136377",
+        CPgreen: "#446d44",
+        CPpurple: "purple",
+        CPblack: "#000",
+        CPwhite: "#fff",
+        CPdark: "#333",
+      },
+      fontFamily: {
+        cyberpunk: "Cyberpunk",
+        oxanium: "Oxanium",
+        blender: "BlenderProBook",
+      },
+    },
   },
   plugins: [],
 };
