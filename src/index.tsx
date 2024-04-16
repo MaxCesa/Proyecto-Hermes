aaea77c6e4f5ab3047030e309b9138278e31abfe
MaxCesa <maxicesa00@gmail.com>
2024-04-16 17:00:49 -0300

    Setup Redux

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./Assets/main.css";
+import { Provider } from "react-redux";
+import { store } from "./Redux/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
